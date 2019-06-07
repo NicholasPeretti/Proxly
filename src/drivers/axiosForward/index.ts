33fda64ff76277afe176a9ee962833a52713ed7d
Nicholas Peretti <peretti.nicholas@gmail.com>
2019-06-07 08:16:20 +0200

    Support invalid https certificates

--- src/drivers/axiosForward/index.ts
+++ src/drivers/axiosForward/index.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import * as https from 'https'
 import ProxyRequest from '../../entities/ProxyRequest'
 
 function axiosResToProxyRequest(res: AxiosResponse): ProxyRequest {
@@ -12,12 +13,21 @@ function axiosResToProxyRequest(res: AxiosResponse): ProxyRequest {
 }
 
 export default function axiosForward(req: ProxyRequest) {
+  /**
+   * Ignore invalid SSL certificates
+   * https://github.com/axios/axios/issues/535
+   */
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  })
+
   return axios
     .request({
       url: req.getTargetUrl(),
       method: req.getMethod(),
       headers: req.getTargetHeaders(),
-      data: req.getTargetBody()
+      data: req.getTargetBody(),
+      httpsAgent: agent
     })
     .then(response => axiosResToProxyRequest(response))
     .catch(error => {
